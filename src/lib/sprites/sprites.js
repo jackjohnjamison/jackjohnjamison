6c89519e7ef6b77123943ae3b199292199fbbc2c
John(Jack) Jamison <jackofthejamison@hotmail.com>
2022-11-04 22:34:49 +0000

    Async load

--- src/lib/sprites/sprites.js
+++ src/lib/sprites/sprites.js
@@ -7,150 +7,191 @@ const imageAssetPath = import.meta.env.PROD
   : "./images/";
 
 sprites.load = async () => {
+  const terracotta1 = loadImage(`${imageAssetPath}terracotta1.png`);
+  const terracotta2 = loadImage(`${imageAssetPath}terracotta2.png`);
+  const terracotta3 = loadImage(`${imageAssetPath}terracotta3.png`);
+  const terracotta4 = loadImage(`${imageAssetPath}terracotta4.png`);
+
+  const cube1 = loadImage(`${imageAssetPath}cube1.png`);
+  const cube2 = loadImage(`${imageAssetPath}cube2.png`);
+  const cube3 = loadImage(`${imageAssetPath}cube3.png`);
+
+  const grass1 = loadImage(`${imageAssetPath}grass1.png`);
+  const grass2 = loadImage(`${imageAssetPath}grass2.png`);
+  const grass3 = loadImage(`${imageAssetPath}grass3.png`);
+  const grass4 = loadImage(`${imageAssetPath}grass4.png`);
+
+  const water1 = loadImage(`${imageAssetPath}water1.png`);
+  const water2 = loadImage(`${imageAssetPath}water2.png`);
+
+  const mountain1 = loadImage(`${imageAssetPath}mountain1.png`);
+  const mountain2 = loadImage(`${imageAssetPath}mountain2.png`);
+  const mountain3 = loadImage(`${imageAssetPath}mountain3.png`);
+
+  const mountainTop1 = loadImage(`${imageAssetPath}mountain1-top.png`);
+  const mountainTop2 = loadImage(`${imageAssetPath}mountain2-top.png`);
+  const mountainTop3 = loadImage(`${imageAssetPath}mountain3-top.png`);
+
+  const forest1 = loadImage(`${imageAssetPath}trees1.png`);
+  const forest2 = loadImage(`${imageAssetPath}trees2.png`);
+  const forest3 = loadImage(`${imageAssetPath}trees3.png`);
+  const forest4 = loadImage(`${imageAssetPath}trees4.png`);
+
+  const forestTop1 = loadImage(`${imageAssetPath}trees1-top.png`);
+  const forestTop2 = loadImage(`${imageAssetPath}trees2-top.png`);
+  const forestTop3 = loadImage(`${imageAssetPath}trees3-top.png`);
+  const forestTop4 = loadImage(`${imageAssetPath}trees4-top.png`);
+
+  const angel = loadImage(`${imageAssetPath}pt-angel.png`);
+  const despoiler = loadImage(`${imageAssetPath}pt-despoiler.png`);
+  const sheild = loadImage(`${imageAssetPath}pt-sheild.png`);
+
+  //////////////////////////////////////////////////
+
   sprites.terracotta = [
     {
       yOffset: 0,
-      data: await loadImage(`${imageAssetPath}terracotta1.png`),
+      data: await terracotta1,
     },
     {
       yOffset: 0,
-      data: await loadImage(`${imageAssetPath}terracotta2.png`),
+      data: await terracotta2,
     },
     {
       yOffset: 0,
-      data: await loadImage(`${imageAssetPath}terracotta3.png`),
+      data: await terracotta3,
     },
     {
       yOffset: 0,
-      data: await loadImage(`${imageAssetPath}terracotta4.png`),
+      data: await terracotta4,
     },
   ];
 
   sprites.cube = [
     {
       yOffset: 33,
-      data: await loadImage(`${imageAssetPath}cube1.png`),
+      data: await cube1,
     },
     {
       yOffset: 33,
-      data: await loadImage(`${imageAssetPath}cube2.png`),
+      data: await cube2,
     },
     {
       yOffset: 33,
-      data: await loadImage(`${imageAssetPath}cube3.png`),
+      data: await cube3,
     },
   ];
 
   sprites.grass = [
     {
       yOffset: 2,
-      data: await loadImage(`${imageAssetPath}grass1.png`),
+      data: await grass1,
     },
     {
       yOffset: 2,
-      data: await loadImage(`${imageAssetPath}grass2.png`),
+      data: await grass2,
     },
     {
       yOffset: 2,
-      data: await loadImage(`${imageAssetPath}grass3.png`),
+      data: await grass3,
     },
     {
       yOffset: 2,
-      data: await loadImage(`${imageAssetPath}grass4.png`),
+      data: await grass4,
     },
   ];
 
   sprites.water = [
     {
       yOffset: -3,
-      data: await loadImage(`${imageAssetPath}water1.png`),
+      data: await water1,
     },
     {
       yOffset: -3,
-      data: await loadImage(`${imageAssetPath}water2.png`),
+      data: await water2,
     },
   ];
 
   sprites.mountain = [
     {
       yOffset: 14,
-      data: await loadImage(`${imageAssetPath}mountain1.png`),
+      data: await mountain1,
     },
     {
       yOffset: 12,
-      data: await loadImage(`${imageAssetPath}mountain2.png`),
+      data: await mountain2,
     },
     {
       yOffset: 14,
-      data: await loadImage(`${imageAssetPath}mountain3.png`),
+      data: await mountain3,
     },
   ];
 
   sprites.mountainTop = [
     {
       yOffset: 14,
-      data: await loadImage(`${imageAssetPath}mountain1-top.png`),
+      data: await mountainTop1,
     },
     {
       yOffset: 12,
-      data: await loadImage(`${imageAssetPath}mountain2-top.png`),
+      data: await mountainTop2,
     },
     {
       yOffset: 14,
-      data: await loadImage(`${imageAssetPath}mountain3-top.png`),
+      data: await mountainTop3,
     },
   ];
 
   sprites.forest = [
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees1.png`),
+      data: await forest1,
     },
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees2.png`),
+      data: await forest2,
     },
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees3.png`),
+      data: await forest3,
     },
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees4.png`),
+      data: await forest4,
     },
   ];
 
   sprites.forestTop = [
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees1-top.png`),
+      data: await forestTop1,
     },
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees2-top.png`),
+      data: await forestTop2,
     },
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees3-top.png`),
+      data: await forestTop3,
     },
     {
       yOffset: 15,
-      data: await loadImage(`${imageAssetPath}trees4-top.png`),
+      data: await forestTop4,
     },
   ];
 
   sprites.playerTokens = {
     angel: {
       yOffset: 51,
-      data: await loadImage(`${imageAssetPath}pt-angel.png`),
+      data: await angel,
     },
     despoiler: {
       yOffset: 51,
-      data: await loadImage(`${imageAssetPath}pt-despoiler.png`),
+      data: await despoiler,
     },
     sheild: {
       yOffset: 48,
-      data: await loadImage(`${imageAssetPath}pt-sheild.png`),
+      data: await sheild,
     },
   };
 };
